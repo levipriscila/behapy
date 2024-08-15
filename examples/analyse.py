30c095d78096cc3a68c1617806092dbbe4137e2d
Chris Nolan <cnolan@gmail.com>
2024-08-15 15:27:49 +1000

    Separate FMM example

--- examples/analyse.py
+++ examples/analyse.py
@@ -7,8 +7,6 @@ from behapy.utils import load_preprocessed_experiment
 from behapy.events import build_design_matrix, regress, find_events
 import statsmodels.api as sm
 import seaborn as sns
-import rpy2.robjects as robjects
-from rpy2.robjects import pandas2ri
 sns.set_theme()
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
@@ -72,62 +70,6 @@ plot_meta = {'Magazine': ['REWmag', 'NOREWmag']}
 event_ids_of_interest = sum(plot_meta.values(), [])
 events_of_interest = events.loc[events.event_id.isin(event_ids_of_interest), :]
 
-# %%
-def _build_fmm_matrix(ev, dff):
-    """
-    Reshape the data into the format requred by the fastFMM R package:
-    Format: animal | session | trial | treatment | Y.1 ... Y.48
-    """
-
-    ev = ev.reset_index(['subject', 'session', 'task', 'run', 'label', 'onset'])
-    ev.onset = pd.to_timedelta(ev.onset).apply(lambda td: td.total_seconds())
-
-    # TODO: The treatment variable is hardcoded to reward / no reward. Change this into a function argument
-    ev['treatment'] = (ev.event_id=='REWmag').astype('int')
-
-    # prepare the resulting data frame
-    fastfmm_data = pd.DataFrame(columns=['id', 'session', 'trial', 'treatment'] + [f'Y.{i+1}' for i in range(48)])
-
-    # counnters and helper variables
-    idx = pd.IndexSlice
-    last_subject = None
-    last_session = None
-    session_counter = 0
-    trial_counter = 0
-
-    # run a loop over all events in the data
-    for event_id, (event_idx, event) in enumerate(ev.iterrows()):
-        if event_id % 1000 == 0: print(f'{event_id} / {ev.shape[0]}')
-
-        # if we've switched to a new subject reset the session counter
-        if last_subject != event.subject:
-            session_counter = 0
-            last_subject = event.subject
-            last_session = None
-
-        # if we've switched to a new session increase the session counter
-        if last_session != event.session:
-            session_counter += 1
-            last_session = event.session
-            trial_counter = 0
-
-        trial_counter += 1
-
-        # add this event's data row to the final data frame
-        row = [event.subject, session_counter, trial_counter, event.treatment] + list(dff.loc[idx[event.subject, event.session, event.task, event.run, event.label, (event.onset - 1.0):(event.onset + 2.0 + 0.2)], 'dff'][:48])
-        fastfmm_data.loc[len(fastfmm_data.index)] = row
-
-    return fastfmm_data
-
-# %%
-fastfmm_matrix = _build_fmm_matrix(ev=events_of_interest, dff=dff)
-
-# %%
-BIDSROOT.joinpath('derivatives', 'fmm').mkdir(parents=True, exist_ok=True)
-fmm_out_file = BIDSROOT/'derivatives'/'fmm'/'rats15_megrew_sessions_trials.csv'
-fastfmm_matrix.to_csv(fmm_out_file, index=False)
-
-
 
 # %%
 def _build_design_matrix(row):
@@ -148,71 +90,6 @@ dm_filt = design_matrix.loc[idx[:, :, ['FI15', 'RR5', 'RR10'], :, :, :], :].sort
 def _regress(df):
     return regress(df, dff.loc[df.index, 'dff'], min_events=25)
 
-# %%
-# activate R magic
-pandas2ri.activate()
-
-# %%
-# Run the R script
-# See https://rpubs.com/gloewinger/1110512 for instructions on running fastFMM
-
-# load R libraries
-with open('../etc/fastFMM_pre.r', 'r') as file:
-    r_script_pre = file.read()
-    robjects.r(r_script_pre)
-
-# read the data into R
-robjects.r(f'dat <- read.csv("{fmm_out_file}")')
-
-# fit the FMM model
-robjects.r('mod <- fui(Y ~ treatment + (1 | id), data=dat, parallel=TRUE, num_cores=4)')
-
-# reshape R output into covenient variables
-with open('../etc/fastFMM_post.r', 'r') as file:
-    r_script_post = file.read()
-    robjects.r(r_script_post)
-
-# %%
-# Collect R varibles back into Python
-
-# collect aic/bic
-mod_aic = pd.DataFrame(robjects.r['mod_aic'])
-mod_aic.columns = ['AIC', 'BIC', 'cAIC']
-
-# collect qn
-mod_qn = robjects.r['mod_qn']
-
-# collect bootstrap samples
-mod_bootsamps = robjects.r['mod_bootsamps']
-
-# collect argument values
-mod_argvals = np.array(robjects.r['mod_argvals'])
-
-# collect coefficients
-mod_betaHat = pd.DataFrame(robjects.r['mod_betahat'])
-
-# collect coefficient variances
-mod_betahat_var = np.array(robjects.r['mod_betahatvar'])
-
-# %%
-lower = mod_betaHat - 2*np.sqrt(np.diagonal(mod_betahat_var, 0, 0, 1))
-upper = mod_betaHat + 2*np.sqrt(np.diagonal(mod_betahat_var, 0, 0, 1))
-lowerj = mod_betaHat - mod_qn[:, np.newaxis]*np.sqrt(np.diagonal(mod_betahat_var, 0, 0, 1))
-upperj = mod_betaHat + mod_qn[:, np.newaxis]*np.sqrt(np.diagonal(mod_betahat_var, 0, 0, 1))
-
-beta_df = mod_betaHat.stack()
-beta_df.name = 'beta'
-beta_df = beta_df.to_frame()
-beta_df['lower'] = lower.stack()
-beta_df['upper'] = upper.stack()
-beta_df['lowerj'] = lowerj.stack()
-beta_df['upperj'] = upperj.stack()
-beta_df.index.names = ['level', 'offset']
-
-# %%
-import seaborn.objects as so
-p1 = so.Plot(beta_df.reset_index(), x='offset', y='beta', ymin='lower', ymax='upper').facet(row='level')
-p1.add(so.Band()).add(so.Line())
 
 # %%
 r1 = dm_filt.loc[:, idx[sum(plot_meta.values(), []), :]].groupby(level=('subject', 'task'), group_keys=True).apply(_regress)
